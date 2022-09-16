954d56427ae7164c976948aa2e456bc4191cfa56
MehmetFK <mehmetfkuruldak@gmail.com>
2022-09-16 22:18:08 +0200

    first commit

---
+++ game.js
@@ -0,0 +1,36 @@
+const car = document.querySelector(".car")
+const ground = document.querySelector(".ground")
+const carImg = document.querySelector(".car img")
+let flag = true
+let flagJump = true
+
+document.body.addEventListener("keydown", (e)=>{
+    if (e.code === "Enter"){
+        if(!ground.classList.contains("right")){
+            ground.classList.add("right")
+            car.classList.add("swing")
+        }else{
+            ground.classList.remove("right")
+            car.classList.remove("swing")
+        }
+    }
+})
+
+document.body.addEventListener("keydown", (e) => {
+    if (e.code === "Escape"){
+        if(flag){
+            carImg.setAttribute("src", "./img/img_06.png")
+            flag = false
+        }else{
+            carImg.setAttribute("src", "./img/Img_05.png")
+            flag = true
+        }
+    }
+})
+document.body.addEventListener("keydown", (e) => {
+    if (e.code === "Space"){
+            car.classList.toggle("jump")
+        }  
+        
+})
+console.log(car.classList.contains("jump"))
